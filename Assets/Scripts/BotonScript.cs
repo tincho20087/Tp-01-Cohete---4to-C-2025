1525bb8d695757f3045efb813cd15abf49730fa8
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2025-03-20 12:11:38 -0300

    Boton script listo

---
+++ Assets/Scripts/BotonScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotonScript : MonoBehaviour
+{
+    public GameObject botonGO;
+
+
+    private void OnMouseDown()
+    {
+        botonGO.SetActive(false);
+        Debug.Log("Click");
+    }
+
+    private void OnMouseUp()
+    {
+        botonGO.SetActive(true);
+    }
+
+}
