6145683f6cbc555071cbb7c8e1478c60aa54f964
tincho20087 <48802391@est.ort.edu.ar>
2025-03-27 11:57:32 -0300

    Update CoheteScript.cs

--- Assets/CoheteScript.cs
+++ Assets/CoheteScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CoheteScript : MonoBehaviour
 {
+    float coheteSpeed = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +15,11 @@ public class CoheteScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0, 0.1f, 0);
+       
+        transform.Translate(0, coheteSpeed, 0);
     }
-    
-}
+    public void despegue()
+    {
+        coheteSpeed = 0.1f;
+        //debe activarse el objeto cohete fuego solo cuando se despegue
+    }
